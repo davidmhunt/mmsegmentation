4bebe3ae3bdb3955b0a25c596110ae69355de924
David Hunt <david.hunt@duke.edu>
2023-11-30 19:30:14 -0500

    adding configurations for the DME dataset

---
+++ configs/ece661/unet-s5-d16_pspnet_4xb4-40k_DME-64x64.py
@@ -0,0 +1,23 @@
+_base_ = ["../unet/unet-s5-d16_pspnet_4xb4-40k_drive-64x64.py"]
+
+#specify the dataset root
+dataset_root = "/data/david/DME"
+train_dataloader = dict(
+    dataset = dict(
+        dataset = dict(
+            data_root = dataset_root
+        )
+    )
+)
+
+val_dataloader = dict(
+    dataset = dict(
+        data_root = dataset_root
+    )
+)
+
+test_dataloader = dict(
+    dataset = dict(
+        data_root = dataset_root
+    )
+)
